eb6c34384fe7ae424474c2d608e8511fa2daa7f3
pmbrull <peremiquelbrull@gmail.com>
2020-11-29 18:30:32 +0100

    reorganization

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+
+class NewTransaction extends StatelessWidget {
+  // This is linked to a TextField controller and saves the input
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              onPressed: () {},
+              textColor: Colors.purple,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
